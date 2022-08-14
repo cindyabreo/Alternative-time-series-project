34128626069bfa705e623ce899a6f6d16c15ab6f
cindyabreo <c.abreoo@gmail.com>
2022-08-14 18:00:16 +0000

    cambios 14 08 2022

--- src/app.py
+++ src/app.py
@@ -1,4 +1,140 @@
-from utils import db_connect
-engine = db_connect()
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.offline as py
+py.init_notebook_mode()
+%matplotlib inline
+plt.style.use('fivethirtyeight')
+from prophet import Prophet
+from prophet.plot import plot_plotly
 
-# your code here
+data_train_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_test_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_train_a.head()
+
+data_train_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_test_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_train_b.head()
+
+data_test_a.info()
+
+data_train_a.reset_index(inplace=True)
+
+#TENEMOS QUE CAMBIAR EL NOMBRE, PORQUE SIEMPRE SE TIENE QUE LLAMAR DS LA COLUMNA.
+data_train_a.rename(columns={'datetime': 'ds', 'cpu': 'y'}, inplace=True)
+
+m = Prophet()
+m.fit(data_train_a)
+
+#PERIODS 1, ES PORQUE SON PREDICCIONES A MINUTO A MINUTO 
+future = m.make_future_dataframe(periods=1)
+future.tail()
+
+# Python
+m = Prophet(weekly_seasonality=False)
+m.add_seasonality(name='hourly', period=60, fourier_order=5)
+forecast = m.fit(data_train_a).predict(future)
+fig = m.plot_components(forecast)
+
+m = Prophet(changepoint_prior_scale=0.01).fit(data_train_a)
+future = m.make_future_dataframe(periods=300, freq='1min')
+fcst = m.predict(future)
+fig = m.plot(fcst)
+
+fig = m.plot_components(fcst)
+
+#otra forma
+m = Prophet(yearly_seasonality=True, weekly_seasonality=True, daily_seasonality=True).fit(data_train_a)
+future = m.make_future_dataframe(periods=300, freq='1min')
+fcst = m.predict(future)
+fig = m.plot(fcst)
+
+m.plot_components(fcst)
+
+data_train_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-a.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_test_a = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-a.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_train_a.head()
+
+data_train_a.index = pd.to_datetime(data_train_a.index)
+data_train_b.index = pd.to_datetime(data_train_b.index)
+
+from pmdarima.arima import auto_arima
+
+#modelo ARIMA
+#arima, con sus porpias observaciones (ar), diferenciación
+from pmdarima.arima import auto_arima
+stepwise_model = auto_arima(data_train_a, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True,
+                           stepwise=True)
+print(stepwise_model.aic())
+
+stepwise_model.fit(data_train_a)
+
+stepwise_model.fit(data_train_a).plot_diagnostics(figsize=(15, 12))
+plt.show()
+
+future_forecast = stepwise_model.predict(n_periods=60)
+
+future_forecast = pd.DataFrame(future_forecast,index = data_test_a.index,columns=['Prediction'])
+pd.concat([data_test_a,future_forecast],axis=1).plot()
+
+#train b
+data_test_b.info()
+
+data_train_b.reset_index(inplace=True)
+
+data_train_b.rename(columns={'datetime': 'ds', 'cpu': 'y'}, inplace=True)
+
+data_train_b.head()
+
+m = Prophet()
+m.fit(data_train_b)
+
+future = m.make_future_dataframe(periods=1)
+future.tail()
+
+m = Prophet(weekly_seasonality=False)
+m.add_seasonality(name='hourly', period=60, fourier_order=5)
+forecast = m.fit(data_train_b).predict(future)
+fig = m.plot_components(forecast)
+
+m = Prophet(changepoint_prior_scale=0.01).fit(data_train_b)
+future = m.make_future_dataframe(periods=300, freq='1min')
+fcst = m.predict(future)
+fig = m.plot(fcst)
+
+m = Prophet(yearly_seasonality=True, weekly_seasonality=True, daily_seasonality=True).fit(data_train_b)
+future = m.make_future_dataframe(periods=300, freq='1min')
+fcst = m.predict(future)
+fig = m.plot(fcst)
+
+future = m.make_future_dataframe(periods=300, freq='1min')
+fcst = m.predict(future)
+m.plot_components(fcst)
+
+data_train_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-train-b.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_test_b = pd.read_csv('https://raw.githubusercontent.com/oreilly-mlsec/book-resources/master/chapter3/datasets/cpu-utilization/cpu-test-b.csv', parse_dates=[0], infer_datetime_format=True,index_col=0)
+data_train_b.head()
+
+stepwise_model = auto_arima(data_train_a, start_p=1, start_q=1,
+                           max_p=3, max_q=3, m=12,
+                           start_P=0, seasonal=True,
+                           d=1, D=1, trace=True,
+                           error_action='ignore',  
+                           suppress_warnings=True, 
+                           stepwise=True)
+print(stepwise_model.aic())
+
+stepwise_model.fit(data_train_b)
+
+stepwise_model.fit(data_train_b).plot_diagnostics(figsize=(15, 12))
+plt.show()
+
+future_forecast = stepwise_model.predict(n_periods=60)
+
+future_forecast = pd.DataFrame(future_forecast,index = data_test_b.index,columns=['Prediction'])
+pd.concat([data_test_b,future_forecast],axis=1).plot()
